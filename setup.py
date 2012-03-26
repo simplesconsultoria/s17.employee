4a6a41c21c9405a1e830b15050c8e0810714cbf8
Alejandro Pereira <alepereira86@gmail.com>
2012-03-26 17:57:38 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='s17.person.employee',
+      version=version,
+      description="A employee content type based on person",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Plone :: 4.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='python plone zope simples_consultoria',
+      author='Simples Consultoria',
+      author_email='products@simplesconsultoria.com.br',
+      url='http://www.simplesconsultoria.com.br',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['s17', 's17.person'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.person',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ src/s17/person/employee/content/employee.py
@@ -0,0 +1,11 @@
+from collective.person.content.person import IPerson
+from collective.person.content.person import Person
+
+
+class IEmployee(IPerson):
+    """ A representation of a Employee
+    """
+
+class Employee(Person):
+    """ Implementation of IEmployee
+    """
