6d8b996781d5c1d05b61d30d819ce74ddfa87c12
Alejandro Pereira <alepereira86@gmail.com>
2012-04-06 02:33:45 -0300

    adding test for edit tabs

--- src/s17/person/employee/tests/test_content.py
+++ src/s17/person/employee/tests/test_content.py
@@ -55,5 +55,62 @@ class IntegrationTest(unittest.TestCase):
         self.assertTrue(IAttributeUUID.providedBy(e1))
 
 
+class FieldsetTest(unittest.TestCase):
+
+    name = 'collective.person.behaviors.contact.IContactInfo'
+    fake_name = 'plone.app.dexterity.behaviors.metadata.IDublinCore'
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.pt = self.portal.portal_types
+        self.pc = self.portal['portal_personcatalog']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        self.folder = self.portal['test-folder']
+        self.folder.invokeFactory('s17.employee', 'e1')
+        behaviors = []
+        behaviors.append(self.name)
+        fti = queryUtility(IDexterityFTI,
+                           name='s17.employee')
+        fti.behaviors = tuple(behaviors)
+
+    def test_fieldsets(self):
+        behaviors = []
+        behaviors.append(self.name)
+        fti = queryUtility(IDexterityFTI,
+                           name='s17.employee')
+        fti.behaviors = tuple(behaviors)
+        e1 = self.folder['e1']
+        request = self.layer['request']
+        request.set('URL', e1.absolute_url() + '/employee_edit')
+        request.set('ACTUAL_URL', e1.absolute_url() + '/employee_edit')
+        edit = e1.restrictedTraverse('employee_edit')
+        setattr(edit, 'portal_type', 's17.employee')
+        edit.updateFieldsFromSchemata()
+        self.assertEquals(edit.groups[0].label, 'Contact Info')
+
+    def test_fake_fieldsets(self):
+        behaviors = []
+        behaviors.append(self.fake_name)
+        fti = queryUtility(IDexterityFTI,
+                           name='s17.employee')
+        fti.behaviors = tuple(behaviors)
+        e1 = self.folder['e1']
+        request = self.layer['request']
+        request.set('URL', e1.absolute_url() + '/employee_edit')
+        request.set('ACTUAL_URL', e1.absolute_url() + '/employee_edit')
+        edit = e1.restrictedTraverse('employee_edit')
+        setattr(edit, 'portal_type', 's17.employee')
+        edit.updateFieldsFromSchemata()
+
+        """We check that our custom edit view is not taking effect
+        with external packages behaviors"""
+        self.assertEquals(edit.groups[0].label, 'Categorization')
+        self.assertEquals(edit.groups[1].label, 'Dates')
+        self.assertEquals(edit.groups[2].label, 'Ownership')
+
+
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
