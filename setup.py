5258a4d1af1d4a8c271da6658bb1c81b4e2081f0
hvelarde <hector.velarde@gmail.com>
2012-05-04 19:42:33 -0300

    updated package information

--- setup.py
+++ setup.py
@@ -1,41 +1,50 @@
 # -*- coding:utf-8 -*-
 
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
 
 version = '0.1'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='s17.person.employee',
       version=version,
       description="A employee content type based on person",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       classifiers=[
         "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
         "Framework :: Plone :: 4.1",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Office/Business",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='python plone zope simples_consultoria',
+      keywords='python plone simples_consultoria',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
       url='http://www.simplesconsultoria.com.br',
       license='GPL',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['s17', 's17.person'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
         'setuptools',
         'collective.person',
-      ],
+        ],
       extras_require={
         'test': ['plone.app.testing'],
-      },
+        },
       entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
