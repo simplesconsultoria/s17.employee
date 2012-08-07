e401d72219c5aac13f045a5e35b49789c7bb0123
hvelarde <hector.velarde@gmail.com>
2012-08-07 17:03:38 -0300

    Preparing release 1.0a5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.0a5.dev0'
+version = '1.0a5'
 description = "A package containing a Dexterity content type and behaviors \
 to represent an Employe on a Plone site."
 long_description = open("README.txt").read() + "\n" + \
