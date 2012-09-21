96af3566fe2855ced37ef65aaa0dde8e414a6d1f
hvelarde <hector.velarde@gmail.com>
2012-09-21 16:53:48 -0300

    update package's i18n

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -17,15 +17,15 @@ for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
 done
 
 # Synchronise the templates and scripts with the .pot.
-#i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/plone.pot \
-#    --create plone \
-#    ${BASE_DIRECTORY}/configure.zcml \
-#    ${BASE_DIRECTORY}/profiles/default
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/plone.pot \
+    --create plone \
+    ${BASE_DIRECTORY}/configure.zcml \
+    ${BASE_DIRECTORY}/profiles/default
 
-# Synchronise the plone's pot file (Used for the workflows)
-#for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/plone.po; do
-#    i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
-#done
+# Synchronise the Plone's pot file (Used for the workflows)
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
+done
 
 # Report of errors and suspect untranslated messages
 i18ndude find-untranslated ${BASE_DIRECTORY}
