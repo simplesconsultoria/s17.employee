d9b33b5c848733e55f20ca83b2676353e90606b9
Juan Pablo Giménez <jpg@rosario.com>
2013-09-06 14:59:57 -0300

    Fix redmine issue #7748, reindex zc.relations catalog

---
+++ src/s17/employee/upgrades/to1001.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from plone.uuid.handlers import addAttributeUUID
+
+from zc.relation.interfaces import ICatalog
+from zope.component import getUtility
+from z3c.relationfield.event import updateRelations
+from z3c.relationfield.interfaces import IHasRelations
+
+from Products.CMFCore.utils import getToolByName
+
+
+PROJECT = 's17.employee'
+
+
+def reindex_zc_relations(context):
+    ''' Upgrade to version 1001
+        reindex zc.relations catalog
+    '''
+    rcatalog = getUtility(ICatalog)
+    # Clear the relation catalog to fix issues with interfaces that don't exist anymore.
+    # This actually fixes the bug editing employees than reports a:
+    #   KeyError: <class 'plone.directives.form.schema.Schema'>
+    rcatalog.clear()
+
+    catalog = getToolByName(context, 'portal_catalog')
+    brains = catalog.searchResults(object_provides=IHasRelations.__identifier__)
+    for brain in brains:
+        obj = brain.getObject()
+        updateRelations(obj, None)
